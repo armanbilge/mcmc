819a6feed6f4b96bf4ba6f05f072d9f2b62163d4
Arman Bilge <armanbilge@gmail.com>
2017-01-24 21:41:41 -0800

    Forgot to extend Operator

--- src/main/scala/mcmc/FocusedOperator.scala
+++ src/main/scala/mcmc/FocusedOperator.scala
@@ -2,7 +2,7 @@ package mcmc
 
 import monocle.Lens
 
-final class FocusedOperator[S, T, @specialized(Double) R](val op: Operator[T, R], val lens: Lens[S, T]) {
+final class FocusedOperator[S, T, @specialized(Double) R](val op: Operator[T, R], val lens: Lens[S, T]) extends Operator[S, R] {
 
   def apply(s: S): S = lens.modify(op)(s)
 
