816ee235dbb9c5b6b06ca6fc18c9123edeaa960a
Arman Bilge <armanbilge@gmail.com>
2017-01-25 17:55:49 -0800

    Bug fix and performance tweak

--- src/main/scala/mcmc/Multinomial.scala
+++ src/main/scala/mcmc/Multinomial.scala
@@ -18,16 +18,19 @@ object Multinomial {
   def apply[T, @specialized(Double) R](probabilities: Map[T, R])(implicit m: Multinomial[T, R]): Dist[T] = m(probabilities)
 
   implicit def any[T, @specialized(Double) R : Ring : Uniform : Order]: Multinomial[T, R] =
-    (probabilities: Map[T, R]) => new DistFromGen[T]({ g =>
-      val u = g.next[R](Uniform(Ring[R].zero, Ring[R].sum(probabilities.values)))
-      val iter = probabilities.iterator
-      var current = iter.next()
-      var cumsum = current._2
-      while (cumsum > u) {
-        current = iter.next()
-        cumsum += current._2
+    (probabilities: Map[T, R]) => {
+      val sortedProbabilities = probabilities.toArray.sortWith(Order.by[(T, R), R](_._2).gt).toIndexedSeq
+      new DistFromGen[T]({ g =>
+        val u = g.next[R](Uniform(Ring[R].zero, Ring[R].sum(probabilities.values)))
+        val iter = sortedProbabilities.toIterator
+        var current = iter.next()
+        var cumsum = current._2
+        while (cumsum > u && iter.hasNext) {
+          current = iter.next()
+          cumsum += current._2
+        }
+        current._1
       }
-      current._1
-    })
+    )}
 
 }
