25a01dbfe8e0b4c7444f8be8a2cdead4fdf3dc43
Arman Bilge <armanbilge@gmail.com>
2017-01-28 01:06:24 -0800

    Define implicit At for Seq

--- src/main/scala/mcmc/implicits.scala
+++ src/main/scala/mcmc/implicits.scala
@@ -2,10 +2,12 @@ package mcmc
 
 import scala.language.implicitConversions
 import monocle.{Iso, Lens}
-import monocle.function.Wrapped
+import monocle.function.{At, Wrapped}
 import monocle.function.all.wrapped
 import shapeless.tag
 import shapeless.tag.@@
+
+import scala.collection.generic.CanBuildFrom
 
 object implicits {
 
@@ -14,5 +16,8 @@ object implicits {
   implicit def tagWrapped[A, T]: Wrapped[A @@ T, A] = new Wrapped[A @@ T, A] {
     val wrapped: Iso[@@[A, T], A] = Iso[@@[A, T], A](identity)(tag.apply[T].apply[A])
   }
+
+  implicit def seqAt[A, S <: Seq[A]](implicit bf: CanBuildFrom[S, A, S]): At[S, Int, A] = (i: Int) =>
+    Lens[S, A](_(i))(x => _.updated[A, S](i, x))
 
 }
