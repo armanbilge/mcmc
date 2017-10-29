fc0fc52cace010dd068256802fb5815ecee00ec1
Arman Bilge <armanbilge@gmail.com>
2017-10-28 17:00:53 -0700

    Bug fix for rng use

--- src/main/scala/mcmc/ScaleOperator.scala
+++ src/main/scala/mcmc/ScaleOperator.scala
@@ -5,7 +5,7 @@ import spire.algebra.{Field, Trig}
 import spire.random.Generator
 import spire.syntax.field._
 
-class ScaleOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: R, val lenses: Traversable[Lens[T, R]])(implicit rng: Generator) extends Operator[T, R] {
+class ScaleOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: R, val lenses: Traversable[Lens[T, R]])(implicit val rng: Generator) extends Operator[T, R] {
 
   override def apply(t: T): T = {
     val scale = scaleFactor + rng.nextDouble * (1 / scaleFactor - scaleFactor)
@@ -20,6 +20,6 @@ class ScaleOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: R
 object ScaleOperator {
 
   implicit def coercer[T, R : Field : Trig]: OperatorCoercer[T, R, ScaleOperator[T, R]] =
-    OperatorCoercer[T, R, ScaleOperator[T, R]](op => Trig[R].log(1 / op.scaleFactor - 1))(x => op => new ScaleOperator[T, R](1 / (Trig[R].exp(x) + 1), op.lenses))
+    OperatorCoercer[T, R, ScaleOperator[T, R]](op => Trig[R].log(1 / op.scaleFactor - 1))(x => op => new ScaleOperator[T, R](1 / (Trig[R].exp(x) + 1), op.lenses)(Field[R], Trig[R], op.rng))
 
 }
--- src/main/scala/mcmc/UpDownOperator.scala
+++ src/main/scala/mcmc/UpDownOperator.scala
@@ -5,7 +5,7 @@ import spire.algebra.{Field, Trig}
 import spire.random.Generator
 import spire.syntax.field._
 
-class UpDownOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: R, val up: Traversable[Lens[T, R]], val down: Traversable[Lens[T, R]])(implicit rng: Generator) extends Operator[T, R] {
+class UpDownOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: R, val up: Traversable[Lens[T, R]], val down: Traversable[Lens[T, R]])(implicit val rng: Generator) extends Operator[T, R] {
 
   override def apply(t: T): T = {
     val scale = scaleFactor + rng.nextDouble * (1 / scaleFactor - scaleFactor)
@@ -20,6 +20,6 @@ class UpDownOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: 
 object UpDownOperator {
 
   implicit def coercer[T, R : Field : Trig]: OperatorCoercer[T, R, UpDownOperator[T, R]] =
-    OperatorCoercer[T, R, UpDownOperator[T, R]](op => Trig[R].log(1 / op.scaleFactor - 1))(x => op => new UpDownOperator[T, R](1 / (Trig[R].exp(x) + 1), op.up, op.down))
+    OperatorCoercer[T, R, UpDownOperator[T, R]](op => Trig[R].log(1 / op.scaleFactor - 1))(x => op => new UpDownOperator[T, R](1 / (Trig[R].exp(x) + 1), op.up, op.down)(Field[R], Trig[R], op.rng))
 
 }
