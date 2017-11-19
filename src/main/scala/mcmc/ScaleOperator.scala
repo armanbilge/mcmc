5751214743ab629d1d11dcff79a107efd7b37afc
Arman Bilge <armanbilge@gmail.com>
2017-11-18 21:25:03 -0800

    Add require statements

--- src/main/scala/mcmc/ScaleOperator.scala
+++ src/main/scala/mcmc/ScaleOperator.scala
@@ -1,11 +1,14 @@
 package mcmc
 
 import monocle.Lens
-import spire.algebra.{Field, Trig}
+import spire.algebra.{Field, Order, Trig}
 import spire.random.Generator
 import spire.syntax.field._
+import spire.syntax.order._
 
-class ScaleOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: R, val lenses: Traversable[Lens[T, R]])(implicit val rng: Generator) extends Operator[T, R] {
+class ScaleOperator[T, @specialized(Double) R : Field : Trig : Order](val scaleFactor: R, val lenses: Traversable[Lens[T, R]])(implicit val rng: Generator) extends Operator[T, R] {
+
+  require(Field[R].zero < scaleFactor && scaleFactor < Field[R].one)
 
   override def apply(t: T): T = {
     val scale = scaleFactor + rng.nextDouble * (1 / scaleFactor - scaleFactor)
@@ -19,7 +22,7 @@ class ScaleOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: R
 
 object ScaleOperator {
 
-  implicit def coercer[T, R : Field : Trig]: OperatorCoercer[T, R, ScaleOperator[T, R]] =
-    OperatorCoercer[T, R, ScaleOperator[T, R]](op => Trig[R].log(1 / op.scaleFactor - 1))(x => op => new ScaleOperator[T, R](1 / (Trig[R].exp(x) + 1), op.lenses)(Field[R], Trig[R], op.rng))
+  implicit def coercer[T, R : Field : Trig : Order]: OperatorCoercer[T, R, ScaleOperator[T, R]] =
+    OperatorCoercer[T, R, ScaleOperator[T, R]](op => Trig[R].log(1 / op.scaleFactor - 1))(x => op => new ScaleOperator[T, R](1 / (Trig[R].exp(x) + 1), op.lenses)(Field[R], Trig[R], Order[R], op.rng))
 
 }
--- src/main/scala/mcmc/UpDownOperator.scala
+++ src/main/scala/mcmc/UpDownOperator.scala
@@ -1,11 +1,14 @@
 package mcmc
 
 import monocle.Lens
-import spire.algebra.{Field, Trig}
+import spire.algebra.{Field, Order, Trig}
 import spire.random.Generator
 import spire.syntax.field._
+import spire.syntax.order._
 
-class UpDownOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: R, val up: Traversable[Lens[T, R]], val down: Traversable[Lens[T, R]])(implicit val rng: Generator) extends Operator[T, R] {
+class UpDownOperator[T, @specialized(Double) R : Field : Trig : Order](val scaleFactor: R, val up: Traversable[Lens[T, R]], val down: Traversable[Lens[T, R]])(implicit val rng: Generator) extends Operator[T, R] {
+
+  require(Field[R].zero < scaleFactor && scaleFactor < Field[R].one)
 
   override def apply(t: T): T = {
     val scale = scaleFactor + rng.nextDouble * (1 / scaleFactor - scaleFactor)
@@ -19,7 +22,7 @@ class UpDownOperator[T, @specialized(Double) R : Field : Trig](val scaleFactor: 
 
 object UpDownOperator {
 
-  implicit def coercer[T, R : Field : Trig]: OperatorCoercer[T, R, UpDownOperator[T, R]] =
-    OperatorCoercer[T, R, UpDownOperator[T, R]](op => Trig[R].log(1 / op.scaleFactor - 1))(x => op => new UpDownOperator[T, R](1 / (Trig[R].exp(x) + 1), op.up, op.down)(Field[R], Trig[R], op.rng))
+  implicit def coercer[T, R : Field : Trig : Order]: OperatorCoercer[T, R, UpDownOperator[T, R]] =
+    OperatorCoercer[T, R, UpDownOperator[T, R]](op => Trig[R].log(1 / op.scaleFactor - 1))(x => op => new UpDownOperator[T, R](1 / (Trig[R].exp(x) + 1), op.up, op.down)(Field[R], Trig[R], Order[R], op.rng))
 
 }
